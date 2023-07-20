9316e51c0b7316d8f38fa5f0d81cc7226c78eb92
Alex Shelley <alexthepeppershelley@gmail.com>
2023-07-20 19:17:07 -0400

    Rewrite movement so you accelerate when diving

--- Scripts/Objects/Player.cs
+++ Scripts/Objects/Player.cs
@@ -7,61 +7,79 @@ namespace FlightSpeedway
     {
         public const float LeftStickDeadzone = 0.1f;
 
-        [Export] public float FlySpeed = 10;
         [Export] public float MaxPitchDegrees = 90;
         [Export] public float MinPitchDegrees = -90;
-        [Export] public float TurnRadius = 16;
+        [Export] public float PitchRotSpeed = 360 * 2;
         [Export] public float PitchReturnToNeutralMult = 0.98f;
 
-        [Export] public float ModelMaxPitch = 45;
-        [Export] public float ModelMaxRoll = 45;
+        [Export] public float YawRotSpeed = 180;
+
+        [Export] public float MinFlySpeed = 5;
+        [Export] public float MaxFlySpeed = 10;
+
+        [Export] public float MaxAccel = 5;
+
         [Export] public float ModelRotDecayRate = 5;
 
         private Node3D _model => GetNode<Node3D>("%Model");
+        private float _pitchRad;
+        private float _yawRad;
+        private float _speed;
 
         public override void _PhysicsProcess(double deltaD)
         {
             float delta = (float)deltaD;
-            Vector2 leftStick = LeftStick();
 
-            float rotSpeed = Mathf.RadToDeg(FlySpeed / TurnRadius);
+            UpdatePitch(delta);
+            UpdateYaw(delta);
+            UpdateSpeed(delta);
 
-            var rotationDegrees = RotationDegrees;
+            Vector3 forward = Vector3.Forward
+                .Rotated(Vector3.Right, _pitchRad)
+                .Rotated(Vector3.Up, _yawRad);
 
-            if (leftStick.Y != 0)
-            {
-                rotationDegrees.X += LeftStick().Y * rotSpeed * delta;
-                rotationDegrees.X = Mathf.Clamp(rotationDegrees.X, MinPitchDegrees, MaxPitchDegrees);
-            }
-            else
-            {
-                rotationDegrees.X *= PitchReturnToNeutralMult;
-            }
+            Velocity = _speed * forward;
 
-            rotationDegrees.Y -= LeftStick().X * rotSpeed * delta;
-
-            RotationDegrees = rotationDegrees;
-            Velocity = -GlobalTransform.Basis.Z * FlySpeed;
             MoveAndSlide();
         }
 
         public override void _Process(double deltaD)
         {
             float delta = (float)deltaD;
+
+            _model.Rotation = new Vector3(_pitchRad, _yawRad, 0);
+        }
+
+        private void UpdatePitch(float delta)
+        {
             var leftStick = LeftStick();
 
-            var targetModelRot = new Vector3(
-                Mathf.Lerp(0, ModelMaxPitch, leftStick.Y),
-                Mathf.Lerp(0, ModelMaxRoll, -leftStick.X),
-                Mathf.Lerp(0, ModelMaxRoll, -leftStick.X)
-            );
+            if (leftStick.Y != 0)
+            {
+                float t = (leftStick.Y + 1) / 2;
+                float pitchDeg = Mathf.Lerp(MinPitchDegrees, MaxPitchDegrees, t);
+                _pitchRad = Mathf.DegToRad(_pitchRad);
+            }
+            else
+            {
+                _pitchRad *= PitchReturnToNeutralMult;
+            }
+        }
 
-            _model.RotationDegrees = DecayToward(
-                _model.RotationDegrees,
-                targetModelRot,
-                ModelRotDecayRate,
-                delta
-            );
+        private void UpdateYaw(float delta)
+        {
+            var leftStick = LeftStick();
+            _yawRad += leftStick.X * Mathf.DegToRad(YawRotSpeed) * delta;
+            _yawRad = Mathf.PosMod(_yawRad, Mathf.DegToRad(360));
+        }
+
+        private void UpdateSpeed(float delta)
+        {
+            float t = Mathf.InverseLerp(MinPitchDegrees, MaxPitchDegrees, _pitchRad);
+            float accel = Mathf.Lerp(MaxAccel, -MaxAccel, t);
+
+            _speed += accel * delta;
+            _speed = Mathf.Clamp(_speed, MinFlySpeed, MaxFlySpeed);
         }
 
         private Vector2 LeftStick()
@@ -77,11 +95,18 @@ namespace FlightSpeedway
             return raw;
         }
 
-        private Vector3 DecayToward(Vector3 from, Vector3 to, float decayRate, float delta)
+        private Vector3 EulerToDir(Vector3 eulerRad)
         {
-            float remaining = from.DistanceTo(to);
-            remaining *= Mathf.Pow(Mathf.E, -decayRate * delta);
-            return to.MoveToward(from, remaining);
+            return Vector3.Forward
+                .Rotated(Vector3.Up, eulerRad.Y)
+                .Rotated(Vector3.Right, eulerRad.X);
+        }
+
+        private float AngleDiff(float fromRad, float toRad)
+        {
+            Vector2 from = Vector2.Up.Rotated(fromRad);
+            Vector2 to = Vector2.Up.Rotated(toRad);
+            return from.AngleTo(to);
         }
     }
 }

8045039172653a427039ca0ee22e116802026c1d
Alex Shelley <alexthepeppershelley@gmail.com>
2023-08-06 00:30:50 -0400

    You can now easily customize the cone shape, but it still looks too...rectangular

--- Scripts/Objects/PlayerFlame.cs
+++ Scripts/Objects/PlayerFlame.cs
@@ -6,7 +6,11 @@ namespace FlightSpeedway
     {
         [Export] public double FlameDuration = 0.4;
         [Export] public double Cooldown = 0.4;
-        [Export] public float FlameDistance = 5;
+        [Export] public float FlameDistance = 3;
+        [Export] public float FlameAngleDeg = 45f;
+        [Export] public int Rows = 3;
+        [Export] public int DoritosPerRow = 3;
+        [Export] public PackedScene DoritoPrefab;
 
         private double _timer;
 
@@ -17,6 +21,27 @@ namespace FlightSpeedway
             CoolingDown
         }
         private State _currentState = State.Ready;
+
+        public override void _Ready()
+        {
+            float degreesPerRow = FlameAngleDeg / Rows;
+            float degreesPerDorito = FlameAngleDeg / DoritosPerRow;
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int d = 0; d < DoritosPerRow; d++)
+                {
+                    var dorito = DoritoPrefab.Instantiate<PlayerFlameDorito>();
+                    dorito.RotationDegrees = new Vector3(
+                        degreesPerRow * r - (FlameAngleDeg / 2),
+                        degreesPerDorito * d - (FlameAngleDeg / 2),
+                        0
+                    );
+
+                    AddChild(dorito);
+                }
+            }
+        }
 
         public void Flame()
         {
